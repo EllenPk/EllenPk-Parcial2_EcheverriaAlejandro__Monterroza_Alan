29cb98d39dcae34d6d30702703c1989b78c5faa2
AlanMonterroza <amonterr10@cuc.edu.co>
2024-10-24 11:04:56 -0500

    segunda version

---
+++ src/app/componentes/contenedor/Cabecera.tsx
@@ -0,0 +1,90 @@
+import { NavLink } from "react-router-dom";
+import miLogo from "../../../assets/img/milogo.png"
+
+export const Cabecera = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <img src={miLogo} alt="El Logo" width="100px"/>
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor02"
+            aria-controls="navbarColor02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Inicio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="*">
+                  Aerolineas
+                </NavLink>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Menus
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink className="dropdown-item" to="/mencree">
+                       Consultar vuelos
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="dropdown-item" to="/menlis">
+                      Horarios
+                    </NavLink>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Rastrear vuelo
+                    </a>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/menuacer">
+                  Acerca de nosotros
+                </NavLink>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-light" type="submit">
+                Buscar
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,8 +1,12 @@
 export const Inicio = () => {
   return (
-    <>
-      Hola este es el del inicio <br />
-      Como el de bienvenida
+      <>
+     <div className="container-fluid py-5">
+      <h1 className="display-5 fw-bold">¡Te esperan los mejores destinos!</h1>
+      <p className="col-md-8 fs-4">Descubre el mundo sin límites.
+      Tu aventura comienza en el aire</p>
+      <button className="btn btn-primary btn-lg" type="button">¡Reserva ahora!</button>
+    </div>
     </>
-  );
-};
+  )
+}
